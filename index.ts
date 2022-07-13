d0f25b17af9c4d85b6eab23104e7364b4d6f872e
CI010 <cijhn@hotmail.com>
2022-07-13 19:52:57 +0800

    Add kook badge

--- index.ts
+++ index.ts
@@ -9,6 +9,13 @@ interface GithubReleaseItem {
     }>
 }
 
+interface KookResponse {
+    open_id: string | null
+    name: string
+    icon: string
+    online_count: string
+}
+
 serve(async (req: Request) => {
     const parsed = new URL(req.url)
     if (parsed.pathname.endsWith('/latest')) {
@@ -16,12 +23,35 @@ serve(async (req: Request) => {
         const response = await fetch('https://api.github.com/repos/voxelum/x-minecraft-launcher/releases?per_page=5', {
             headers: {
                 'Accept': 'application/vnd.github.v3+json',
-               'Authorization': `token ${Deno.env.get('GITHUB_PAT')}`,
+                'Authorization': `token ${Deno.env.get('GITHUB_PAT')}`,
             }
         })
         const releases: GithubReleaseItem[] = await response.json()
         const filtered = releases.filter(v => includePrerelease ? true : !v.prerelease)[0]
         return Response.json(filtered)
+    }
+    // if (parsed.pathname.endsWith('/afdian-badge')) {
+    //     return Response.json({
+    //         schemaVersion: 1,
+    //         label: "爱发电",
+    //         color: "946ce6",
+    //         message: content.online_count,
+    //      })
+    // }
+    if (parsed.pathname.endsWith('/kook-badge')) {
+        const response = await fetch("https://kookapp.cn/api/guilds/2998646379574089/widget.json")
+        const content: KookResponse = await response.json()
+        const decoder = new TextDecoder('utf-8')
+        const fileContent = await Deno.readFile('./favicon-kook.svg')
+        const svg = decoder.decode(fileContent)
+        
+        return Response.json({
+           schemaVersion: 1,
+           label: " ",
+           logoSvg: svg,
+           message: content.online_count,
+           labelColor: "87eb00"
+        })
     }
     return Response.json({ error: 'Not Found' }, { status: 404 })
 });
