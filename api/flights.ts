e77f0063c5ac53c66ebae6e184286aaec2ea9aa1
CI010 <cijhn@hotmail.com>
2024-10-25 19:55:16 +0800

    enable flights

--- api/flights.ts
+++ api/flights.ts
@@ -1,6 +1,22 @@
 import { Router } from "oak";
 
 export default new Router().get("/flights", (ctx) => {
-  ctx.response.body = {
-  };
+  const params = ctx.request.url.searchParams;
+  const version = params.get("version");
+  const locale = params.get("locale");
+  const build = params.get("build");
+
+  if (!version || !locale) {
+    ctx.response.body = {}
+    return;
+  }
+
+  if (build && Number(build) > 1002) {
+    ctx.response.body = {
+      i18nSearch: ['zh-CN']
+    }
+  } else {
+    ctx.response.body = {
+    };
+  }
 });
