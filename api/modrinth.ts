eb69d25d0b03c7b4522f57d4c1f00c7a0ad408f1
ci010 <cijhn@hotmail.com>
2025-04-06 16:27:15 +0800

    adjust mr auth api

--- api/modrinth.ts
+++ api/modrinth.ts
@@ -3,18 +3,19 @@ import { Router } from "oak";
 export default new Router().get("/modrinth/auth", async (ctx) => {
     const request = ctx.request;
 
-    const response = await fetch("https://api.modrinth.com/_internal/oauth/token", {
+    const url = new URL("https://api.modrinth.com/_internal/oauth/token");
+    url.searchParams.set("client_id", "GFz0B21y");
+    url.searchParams.set("redirect_uri", request.url.searchParams.get("redirect_uri") || "");
+    url.searchParams.set("code", request.url.searchParams.get("code") || "");
+    url.searchParams.set("grant_type", "authorization_code");
+
+    const response = await fetch(url, {
         method: "POST",
         headers: {
-            ["Content-Type"]: "application/json",
             Authorization: Deno.env.get('MODRINTH_SECRET') || "",
+            'Content-Type': 'application/x-www-form-urlencoded',
+            ['User-Agent']: ctx.request.headers.get('User-Agent') || "",
         },
-        body: JSON.stringify({
-            code: request.url.searchParams.get("code"),
-            client_id: "GFz0B21y",
-            redirect_uri: request.url.searchParams.get("redirect_uri"),
-            grant_type: "authorization_code",
-        })
     })
 
     ctx.response.status = response.status;
