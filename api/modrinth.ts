d9c994de12278cca06f40b853174963f788d426b
ci010 <cijhn@hotmail.com>
2025-04-06 16:29:04 +0800

    adjust query string

--- api/modrinth.ts
+++ api/modrinth.ts
@@ -4,11 +4,6 @@ export default new Router().get("/modrinth/auth", async (ctx) => {
     const request = ctx.request;
 
     const url = new URL("https://api.modrinth.com/_internal/oauth/token");
-    url.searchParams.set("client_id", "GFz0B21y");
-    url.searchParams.set("redirect_uri", request.url.searchParams.get("redirect_uri") || "");
-    url.searchParams.set("code", request.url.searchParams.get("code") || "");
-    url.searchParams.set("grant_type", "authorization_code");
-
     const response = await fetch(url, {
         method: "POST",
         headers: {
@@ -16,6 +11,12 @@ export default new Router().get("/modrinth/auth", async (ctx) => {
             'Content-Type': 'application/x-www-form-urlencoded',
             ['User-Agent']: ctx.request.headers.get('User-Agent') || "",
         },
+        body: new URLSearchParams({
+            client_id: "GFz0B21y",
+            redirect_uri: request.url.searchParams.get("redirect_uri") || "",
+            code: request.url.searchParams.get("code") || "",
+            grant_type: "authorization_code",
+        })
     })
 
     ctx.response.status = response.status;
