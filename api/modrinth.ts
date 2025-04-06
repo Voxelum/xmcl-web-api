f4a8feb8d0327abcdac7d718bca2875443fc4924
ci010 <cijhn@hotmail.com>
2025-04-06 16:13:16 +0800

    adjust the mr auth request

--- api/modrinth.ts
+++ api/modrinth.ts
@@ -6,6 +6,7 @@ export default new Router().get("/modrinth/auth", async (ctx) => {
     const response = await fetch("https://api.modrinth.com/_internal/oauth/token", {
         method: "POST",
         headers: {
+            ["Content-Type"]: "application/json",
             Authorization: Deno.env.get('MODRINTH_SECRET') || "",
         },
         body: JSON.stringify({
