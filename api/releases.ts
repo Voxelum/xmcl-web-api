069a4a6a30465fcffd7da5c5dac7ad6bb0ed0e60
ci010 <cijhn@hotmail.com>
2025-04-30 17:39:54 +0800

    log the release api

--- api/releases.ts
+++ api/releases.ts
@@ -5,5 +5,7 @@ export default new Router().get("/releases/:filename", async (ctx) => {
     // redirect to github releases
     const fileName = ctx.params.filename;
     const version = fileName.split('-')[1];
+    const headers = ctx.request.headers;
+    console.log('getting file', fileName, Object.fromEntries(headers.entries()))
     ctx.response.redirect(`https://github.com/Voxelum/x-minecraft-launcher/releases/download/v${version}/${fileName}`);
 })
